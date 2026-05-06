27f36949e59822628882877c1a2e2cbba86323e0
ikboljonabdumalikov <happinessjonabdumalikov@gmail.com>
2026-05-07 00:11:21 +0900

    Declaring Initial values

--- Program.cs
+++ Program.cs
@@ -1,45 +1,8 @@
 ﻿
-bool isRunning = true;
-while (isRunning)
-{
-    Console.Write("\nFaqat + va - amallari bor misol kiriting: ");
-    string input = Console.ReadLine();
+Console.WriteLine("Ifodani kiriting (masalan, 1+3-1+5+8):");
+string input = Console.ReadLine();
 
-    if (string.IsNullOrEmpty(input)) continue;
-
-    int i = 0;
-    // 1. Birinchi sonni ajratib olish
-    string tempNum = "";
-    while (i < input.Length && (input[i] >= '0' && input[i] <= '9'))
-    {
-        tempNum += input[i];
-        i++;
-    }
-    double result = double.Parse(tempNum);
-
-    // 2. Qolgan amallarni bajarish
-    while (i < input.Length)
-    {
-        char op = input[i]; // Amallarni olamiz (+ yoki -)
-        i++;
-
-        string nextNumStr = "";
-        while (i < input.Length && (input[i] >= '0' && input[i] <= '9'))
-        {
-            nextNumStr += input[i];
-            i++;
-        }
-
-        if (nextNumStr != "")
-        {
-            double nextNum = double.Parse(nextNumStr);
-            if (op == '+') result += nextNum;
-            else if (op == '-') result -= nextNum;
-        }
-    }
-
-    Console.WriteLine("Javob: " + result);
-
-    Console.Write("\nYana (ha/yo'q): ");
-    if (Console.ReadLine().ToLower() != "ha") isRunning = false;
-}
+int result = 0;
+int currentNumber = 0;
+char lastOp = '+';
+int i = 0;
